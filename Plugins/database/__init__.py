a295f4a1e482c7cf1cf49cff9185451ebe775fa2
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-08-11 16:45:34 +0530

    Update __init__.py

--- Plugins/database/__init__.py
+++ Plugins/database/__init__.py
@@ -2,9 +2,7 @@ from config import MONGO_DB_URI,NEW_USER_BONUS,REFER_BONUS
 from Plugins import JN
 from pymongo import MongoClient
 client = MongoClient(MONGO_DB_URI)
-db = client['user_balances']
-collection = db['balances']
-files_collection = db["files"]
+
 
 
 # Function to update balance for a user
