36831ba650f0cb1c779268209a707508eff9c4bd
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-08-19 06:40:38 +0530

    Update commands.py

--- Plugins/modules/commands.py
+++ Plugins/modules/commands.py
@@ -40,6 +40,42 @@ async def broadcast_command_handler(b, m: Message):
         await m.reply("please reply any message to broadcast")
         
         
+        
+@JN.on_message(filters.private & filters.user(OWNER_ID) & filters.command(["forward"]))
+async def broadcast_command_handler(b, m: Message):
+    # Check if the user is authorized to use the /broadcast command
+    if m.reply_to_message:
+        ms = await m.reply("Getting All ids from database...\nPlease wait")
+        
+        # Retrieve all user IDs from the database
+        user_ids = [doc['user_id'] for doc in collection.find({}, {'_id': 0, 'user_id': 1})]
+        tot = len(user_ids)
+        success = 0
+        failed = 0
+        successid=["userid:"]
+        await ms.edit(f"Starting Broadcast...\nSending Message To {tot} Users")
+        
+        # Send the broadcast message to all users
+        for user_id in user_ids:
+            try:
+                if(int(user_id)==OWNER_ID):
+                    continue
+                await b.forward_messages(chat_id=int(user_id), from_chat_id=m.chat.id, message_ids=m.reply_to_message.message_id)
+                #await m.reply_to_message.copy(chat_id=int(user_id))
+                
+                success += 1
+            except Exception as e:
+                failed += 1
+                # Handle specific exceptions if needed
+                
+            
+                await ms.edit(f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message.\nTotal - {tot} ")
+            
+        
+        
+    else:
+        await m.reply("please reply any message to broadcast")
+        
 
 @JN.on_message(filters.private & filters.command(["stats"]))
 async def broadcast_command_handler(b, m: Message):
