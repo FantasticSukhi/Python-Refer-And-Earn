8829cc06da59ab4d0f7828b5e55e02b5d183d3e0
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2024-12-03 19:25:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 from pyrogram.types import KeyboardButton, InlineKeyboardMarkup,InlineKeyboardButton,WebAppInfo,ReplyKeyboardMarkup
 
 #get from https://my.telegram.org/auth
-API_ID:int =(os.environ.get("API_ID",21714376))
-API_HASH:str = os.environ.get("API_HASH", "700092e37d7da9a7b781994b7503a4")
-BOT_TOKEN:str = os.environ.get("BOT_TOKEN", "")
+API_ID:int =(os.environ.get("API_ID",20353207))
+API_HASH:str = os.environ.get("API_HASH", "e5b2ac2f9c37bda345fa9fb5ade66961")
+BOT_TOKEN:str = os.environ.get("BOT_TOKEN", "7634909458:AAE1uIsxWXxoB88zrC9kYrR5zWtIN-0w1A4")
 
 
 REFER_BONUS=int(1)
@@ -14,24 +14,24 @@ WITHDRAWAL=int(2)
 
 
 #username without @
-UPDATE_CHNL:str = os.environ.get("UPDATE_CHNL", "jn_bots")
-SUPPORT_GRP:str = os.environ.get("SUPPORT_GRP", "-1002195994803")
+UPDATE_CHNL:str = os.environ.get("UPDATE_CHNL", "OFFICIAL_MAMBA_NETWORK")
+SUPPORT_GRP:str = os.environ.get("SUPPORT_GRP", "-1002138809373")
 
 #get it from @username_to_id_bot this bot 
 
-log_channel = int(os.environ.get("LOG_CHANNEL", "-1001918482012"))
-OWNER_ID=5597521952
+log_channel = int(os.environ.get("LOG_CHANNEL", "-1002156100590"))
+OWNER_ID=6713994904
 
 
 # search on youtube "how to create mongodburl""
 
 MONGO_DB_URI:str = os.environ.get(
     "MONGO_DB_URI",
-    "")
+    "mongodb+srv://danishzain1637:papaspartan@cluster0.xdje3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 	
 
 # all channels list you can add more 
-links = ["https://t.me/jn_bots", "https://t.me/jn_family", "https://t.me/channel3"]
+links = ["https://t.me/MBV_NETWORK", "https://t.me/OFFICIAL_MAMBA_NETWORK", "https://t.me/OFFICIAL_MAMBA_SUPPORT"]
 
 
 
